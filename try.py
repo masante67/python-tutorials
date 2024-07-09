7200c8ada49437e23f46f80eac20235baaefa4e7
masante67 <asantemicheliell@gmail.com>
2024-07-09 15:25:32 +0000

    new python files

---
+++ try.py
@@ -0,0 +1,16 @@
+age = int(input('What is your age? '))
+print(age)
+print("I am", age)
+
+#is about a country
+europe = input("what is the europe floor number? ")
+usa = int(europe)-2
+print(usa, " is the usa floor number")
+
+hours = int(input("how many hours did you work today"))
+rate = int(input("enter rate"))
+pay = hours * rate
+print("your salary is", pay)
+
+
+
